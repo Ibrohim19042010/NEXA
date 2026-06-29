d3a9ab4bc50dcef5d2351702da31038b6558d789
Ibrohim19042010 <157272415+Ibrohim19042010@users.noreply.github.com>
2026-06-29 20:56:50 +0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,33 @@
-js
+// js/utils.js
+
+// Telegram Web App obyekti
+export const tg = window.Telegram.WebApp;
+
+// Oynani to'liq ekranga ochish
+tg.expand();
+
+// Foydalanuvchi ma'lumotlarini olish
+export const getUserData = () => {
+    if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+        return tg.initDataUnsafe.user;
+    }
+    // Agar kompyuterda (brauzerda) test qilinayotgan bo'lsa, fake ma'lumot beramiz
+    return {
+        id: 123456789,
+        first_name: "Hunter",
+        username: "solo_player"
+    };
+};
+
+// Sonlarni chiroyli formatlash (masalan: 10000 -> 10 000)
+export const formatNumber = (num) => {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+};
+
+// Elementni yashirish / ko'rsatish
+export const toggleVisibility = (elementId, show) => {
+    const el = document.getElementById(elementId);
+    if (el) {
+        show ? el.classList.remove('hidden') : el.classList.add('hidden');
+    }
+};
