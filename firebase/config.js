94e6f28de8036f20f825a6a4dbf99c9a96b1feed
Ibrohim19042010 <157272415+Ibrohim19042010@users.noreply.github.com>
2026-06-30 15:44:46 +0500

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,1 +1,7 @@
+// firebase/config.js
 
+// Siz taqdim etgan Firebase Realtime Database URL manzili
+export const FIREBASE_DB_URL = "https://nexaanimebot-default-rtdb.firebaseio.com";
+
+// Agar kelajakda rasmlarni saqlash uchun Storage ishlatsangiz, bu yerga yoziladi
+export const FIREBASE_STORAGE_URL = ""; 
